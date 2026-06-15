3f09d6b0833c40b8cdd41e6bf91aca4bdd69b0fc
Danila Poyarkov <dev@dannote.net>
2026-06-15 20:41:45 +0200

    chore(release): add 0.1.0 native checksums

---
+++ checksum-Elixir.Skia.Native.exs
@@ -0,0 +1,5 @@
+%{
+  "libskia_native-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:e50fcf3d391589667845b3c1e2a3b9bf4df9ebc05f605e02d9f62ccb564e78af",
+  "libskia_native-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:39bc13ab96c40dfb11920cd936fc25ecb99af83d24b48d51250b2264896bfbbb",
+  "libskia_native-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d9befa3c00818ee2dd71611ea33a9f9cf9b16a8298daf340625ef1429dfda03f",
+}
