92a78e78f442ccc22c8efe3050724c14d138f9e4
Danila Poyarkov <dev@dannote.net>
2026-06-18 10:42:23 +0200

    Assert native schema docs and types

--- test/skia/codegen_native_schema_test.exs
+++ test/skia/codegen_native_schema_test.exs
@@ -5,12 +5,33 @@ defmodule Skia.CodegenNativeSchemaTest do
     assert %RustQ.Syn.Method{
              name: "draw_rect",
              visibility: :public,
+             docs: ["Draws [`Rect`] rect using clip, [`Matrix`], and [`Paint`] `paint`." | _],
              args: [
-               %RustQ.Syn.Arg{name: "self", type: "& self"},
-               %RustQ.Syn.Arg{name: "rect", type: "impl AsRef < Rect >"},
-               %RustQ.Syn.Arg{name: "paint", type: "& Paint"}
+               %RustQ.Syn.Arg{
+                 name: "self",
+                 type: "& self",
+                 type_ast: %RustQ.Syn.Type.Ref{inner: %RustQ.Syn.Type.Self{}}
+               },
+               %RustQ.Syn.Arg{
+                 name: "rect",
+                 type: "impl AsRef < Rect >",
+                 type_ast: %RustQ.Syn.Type.ImplTrait{
+                   traits: [
+                     %RustQ.Syn.Type.Path{
+                       name: "AsRef",
+                       args: [%RustQ.Syn.Type.Path{name: "Rect"}]
+                     }
+                   ]
+                 }
+               },
+               %RustQ.Syn.Arg{
+                 name: "paint",
+                 type: "& Paint",
+                 type_ast: %RustQ.Syn.Type.Ref{inner: %RustQ.Syn.Type.Path{name: "Paint"}}
+               }
              ],
-             returns: "& Self"
+             returns: "& Self",
+             returns_ast: %RustQ.Syn.Type.Ref{inner: %RustQ.Syn.Type.Self{}}
            } = Skia.Codegen.NativeSchema.method!("Canvas", "draw_rect")
   end
 end
