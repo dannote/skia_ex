bd0ab20846c3effb5494fec371233a3b1ebf6b31
Danila Poyarkov <dev@dannote.net>
2026-07-18 08:13:16 +0200

    Add 0.3.5 precompiled checksums

--- checksum-Elixir.Skia.Native.exs
+++ checksum-Elixir.Skia.Native.exs
@@ -1,5 +1,5 @@
 %{
-  "libskia_native-v0.3.4-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:7ee1cb050c4fd5f6f0f9e6770567e433cbd28c82d8f778599e9b48be054f5e5a",
-  "libskia_native-v0.3.4-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:6b15e3fa3394dc8fb2ab58af309a3e44452b46262303a123ece08245072ff11d",
-  "libskia_native-v0.3.4-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:19878db42ff6546546b04e30bac0ac1868da755b5fbd9a24584446503d72d642",
+  "libskia_native-v0.3.5-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2580d8bbe0c84469e43d15fe7cd50e3b7ec76a3d492b01c7da36fd2473ef1bd3",
+  "libskia_native-v0.3.5-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:7440964f0df52158ded4da0488becde0bff87f8be1b0f019ae46f476b4c31e66",
+  "libskia_native-v0.3.5-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4807daa307432133f805994f4063bb998b4c2ae49b4a6c7110988379083cf26e",
 }
